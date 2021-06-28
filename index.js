426fdd157d209f68a68319e3e2e9c458407cf347
Nightcoder <newnightcoder@gmail.com>
2021-06-28 23:35:36 +0200

    numeraljs/searchbar :wrench:

--- index.js
+++ index.js
@@ -43,29 +43,26 @@ const selectProduct = () => {
 const displayProducts = () => {
   cameras.map((item) => {
     const product = `
-    <div class="product card mb-3">
-      <a class="link-dark link-product" href="produit.html" >
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${
-              item.imageUrl
-            }" class=" img img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body text-dark ">
-              <h5 class="card-title">${item.name}</h5>
-              <p class="card-text text-end" style="font-weight:700">${numeral(
-                item.price
-              )
-                .divide(100)
-                .format("0,0.00")}€ </p>
-              <p class="card-text">${item.description}</p>
-              <div class="text-end"><button class="btn btn-sm btn-info justify-right" type="button">Voir ce produit</button></div>
-            </div>
-          </div>
+  <div class="product card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img 
+        src="${item.imageUrl}" class=" img img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body text-dark ">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text text-end" style="font-weight:700">${numeral(
+            item.price
+          )
+            .divide(100)
+            .format("0 0.00")}€ </p>
+          <p class="card-text">${item.description}</p>
+          <div class="text-end"><a href="produit.html" class="btn btn-sm btn-info" type="button">Voir ce produit</a></div>
         </div>
-      </a>
+      </div>
     </div>
+  </div>
 `;
 
     content.insertAdjacentHTML("beforeend", product);
