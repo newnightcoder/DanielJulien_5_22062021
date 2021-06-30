64a95a08514d1a6ba8b6fb0ae537d21ebfc1d81c
Nightcoder <newnightcoder@gmail.com>
2021-06-30 04:48:41 +0200

    some styling page panier :art:

--- panier.js
+++ panier.js
@@ -1,15 +1,14 @@
 const content = document.querySelector(".recap");
 const cartNumberStorage = JSON.parse(localStorage.getItem("cartNumberStorage"));
 const finalCartStorage = JSON.parse(localStorage.getItem("finalCartStorage"));
+const recapContent = document.querySelector(".recap-content");
 let number = cartNumberStorage;
 
-const recap = cartNumberStorage
-  ? `<div> 
-      <span class="quantité"></span> 
-      <span class="msg-articles">articles</span> dans mon panier
-      <button class="btn btn-sm btn-danger ms-3">vider le panier</button>
-      </div>`
-  : `<div>Panier vide</div>`;
+const recap = `<div class="container mt-2 mb-5"> 
+                <span class="quantité"></span> 
+                <span class="msg-articles">articles</span> dans mon panier
+                <button class="btn btn-sm btn-danger ms-3">vider le panier</button>
+              </div>`;
 
 const displayRecap = () => {
   if (!finalCartStorage) return;
@@ -31,51 +30,56 @@ const displayRecap = () => {
       <button class="btn btn-sm btn-dark plus">+</button>
       <button class="btn btn-sm btn-dark suppr">supprimer</button>
     </div></td>
-    <td style="white-space:nowrap; text-transform:uppercase; font-weight:600; color:red">${numeral(
-      finalCartStorage[i][1].price
-    )
-      .divide(100)
-      .format("0 0.00")}€</td>
+    <td style="white-space:nowrap; text-transform:uppercase; font-weight:600; color:red">${
+      numeral(finalCartStorage[i][1].price).divide(100).format("0 0.00") *
+      finalCartStorage[i][0]
+    }€</td>
    </tr>`;
-      document
-        .querySelector(".recap-content")
-        .insertAdjacentHTML("afterbegin", recapTable);
+      recapContent.insertAdjacentHTML("afterbegin", recapTable);
     }
   }
 };
 
 const displayQuantity = () => {
-  content.insertAdjacentHTML("beforeEnd", recap);
-  document.querySelector(".quantité").innerHTML = cartNumberStorage;
+  cartNumberStorage
+    ? (content.insertAdjacentHTML("beforeEnd", recap),
+      (document.querySelector(".quantité").innerHTML = cartNumberStorage))
+    : recapContent.insertAdjacentHTML(
+        "afterBegin",
+        `<div style="text-transform:uppercase; font-weight:bold; text-align:center">Panier vide...</div>`
+      );
   if (cartNumberStorage === 1) {
     document.querySelector(".msg-articles").textContent = "article";
   }
 };
 
 const plus = () => {
-  document.querySelector(".plus").addEventListener("click", () => {
-    number++;
-    localStorage.setItem("cartNumberStorage", JSON.stringify(number));
-    document.querySelector(".quantité").innerHTML = number;
-  });
+  cartNumberStorage &&
+    document.querySelector(".plus").addEventListener("click", () => {
+      number++;
+      localStorage.setItem("cartNumberStorage", JSON.stringify(number));
+      document.querySelector(".quantité").innerHTML = number;
+    });
 };
 
 const moins = () => {
-  document.querySelector(".moins").addEventListener("click", () => {
-    if (number === 0) return;
-    number--;
-    localStorage.setItem("cartNumberStorage", JSON.stringify(number));
-    console.log(number);
-    document.querySelector(".quantité").innerHTML = number;
-  });
+  cartNumberStorage &&
+    document.querySelector(".moins").addEventListener("click", () => {
+      if (number === 0) return;
+      number--;
+      localStorage.setItem("cartNumberStorage", JSON.stringify(number));
+      console.log(number);
+      document.querySelector(".quantité").innerHTML = number;
+    });
 };
 
 const clear = () => {
   localStorage.clear();
-  content.textContent = "Panier vide";
+  recapContent.innerHTML = `<div style="text-transform:uppercase">Panier vide</div>`;
 };
 const suppr = () => {
-  document.querySelector(".suppr").addEventListener("click", clear);
+  cartNumberStorage &&
+    document.querySelector(".btn-danger").addEventListener("click", clear);
 };
 
 const initPage = (() => {
