98cf5a708bbd5ec20f7ee3946e06a5f33ae4bbd5
Nightcoder <newnightcoder@gmail.com>
2021-07-19 08:21:13 +0200

    réorganisation fichiers :card_file_box:

---
+++ client-side/index.js
@@ -0,0 +1,82 @@
+const content = document.querySelector(".content");
+const cart = document.querySelector(".badge");
+
+const API_URL = "http://localhost:3000/api/cameras";
+let cameras = [];
+
+const storage = JSON.parse(localStorage.getItem("selectedProduct"));
+const cartNumberStorage = JSON.parse(localStorage.getItem("cartNumberStorage"));
+console.log(typeof storage);
+const displayCartNumberStorage = () => {
+  cartNumberStorage
+    ? ((cart.innerHTML = cartNumberStorage), (cart.style.display = "block"))
+    : (cart.style.display = "none");
+};
+
+// let's get the cameras from the API and store the data in the cameras array.
+const getCameras = async () => {
+  try {
+    const results = await fetch(API_URL);
+    const data = await results.json();
+    cameras = [...data];
+  } catch (error) {
+    console.log(error);
+  }
+  return cameras;
+};
+
+const selectProduct = () => {
+  let productIndex;
+  let selectedProduct;
+  document.querySelectorAll(".btn-voir").forEach((item, i) => {
+    item.addEventListener("click", () => {
+      productIndex = i;
+      // pour changer un peu des for loop! (for of loop avec recherche de l'index)
+      for (const [i, item] of cameras.entries()) {
+        if (productIndex === i) {
+          selectedProduct = [1, item];
+        }
+      }
+      console.log(selectedProduct);
+      localStorage.setItem("selectedProduct", JSON.stringify(selectedProduct));
+    });
+  });
+};
+
+//let's display the data once it is fetched
+const displayProducts = () => {
+  cameras.map((item) => {
+    const product = `
+  <div class="product card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img 
+        src="${item.imageUrl}" class=" img img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body text-dark ">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text text-end" style="font-weight:700">${numeral(
+            item.price
+          )
+            .divide(100)
+            .format("0 0.00")}€ </p>
+          <p class="card-text">${item.description}</p>
+          <div class="text-end"><a href="./client-side/pages/produit/produit.html" class="btn btn-sm btn-voir" type="button">Voir ce produit</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+    content.insertAdjacentHTML("beforeend", product);
+  });
+};
+
+const initPage = (async () => {
+  cameras = await getCameras();
+  console.log("init cameras", cameras);
+  displayProducts();
+  selectProduct();
+  displayCartNumberStorage();
+})();
---
+++ client-side/pages/produit/produit.js
@@ -0,0 +1,123 @@
+const content = document.querySelector(".detail");
+const aside = document.querySelector("aside");
+const select = document.querySelector("select");
+const orderBtn = document.querySelector(".order-btn");
+const cart = document.querySelector(".badge");
+const breadcrumb = document.querySelector("[aria-current=page]");
+const modal = document.querySelector(".modal-body");
+
+const cartNumberStorage = JSON.parse(localStorage.getItem("cartNumberStorage"));
+const selectedProduct = JSON.parse(localStorage.getItem("selectedProduct"));
+const finalCartStorage = JSON.parse(localStorage.getItem("finalCartStorage"));
+// console.log(finalCartStorage[0]);
+
+const detail =
+  selectedProduct &&
+  `<div class="col-lg-8">
+    <h3 class="">${selectedProduct[1].name}</h3>
+    <div class="">
+    <img height="350"  width="500" src="${selectedProduct[1].imageUrl}"/>
+    </div>
+    <div class="">
+    <p style="width:80%" class="py-4">${selectedProduct[1].description} </p>
+    </div>
+   </div>`;
+
+const prix =
+  selectedProduct &&
+  `<div class="row align-items-center justify-content-between">
+    <span class="price col-4" style="color:red;font-size:1.75rem">
+    ${numeral(selectedProduct[1].price).divide(100).format("0 0.00")}€
+    </span> 
+    <span class="dispo col-4" style="white-space:nowrap"> <i class="bi bi-check2"></i>disponible</span>
+   </div>`;
+
+const modalContent =
+  selectedProduct &&
+  `<table class="table table-borderless my-3">
+     <tbody>
+      <tr class="modal-row align-middle">
+      <td><img class="img-fluid" width="150" src="${
+        selectedProduct[1].imageUrl
+      }"/></td>
+      <td><span style="white-space:nowrap; text-transform:uppercase; font-weight:600">${
+        selectedProduct[1].name
+      }</span><br><span style="font-size:.75rem; text-align:left">vendu et exp&eacute;di&eacute; par Orinico</span></td>
+      <td style="white-space:nowrap; text-transform:uppercase; font-weight:600; color:red">${numeral(
+        selectedProduct[1].price
+      )
+        .divide(100)
+        .format("0 0.00")}€</td>
+      </tr>
+     </tbody>
+   </table>`;
+
+const displayProductPage = () => {
+  const selectOptions =
+    selectedProduct &&
+    selectedProduct[1].lenses.map((lens) => `<option>${lens}</option>`);
+  content.insertAdjacentHTML("afterBegin", detail);
+  select.insertAdjacentHTML("beforeEnd", selectOptions);
+  aside.insertAdjacentHTML("afterbegin", prix);
+  breadcrumb.textContent = selectedProduct && `${selectedProduct[1].name}`;
+  modal.insertAdjacentHTML("afterbegin", modalContent);
+};
+
+const addToCart = () => {
+  //ajouter au nombre total d'items dans le cart (storage + view)
+  let cartNumber = cartNumberStorage;
+  orderBtn.addEventListener("click", () => {
+    cartNumber++;
+    localStorage.setItem("cartNumberStorage", JSON.stringify(cartNumber));
+    cart.innerHTML = cartNumber;
+    cart.style.display = "block";
+  });
+};
+
+let finalCart = [...selectedProduct];
+
+const addToStorage = () => {
+  orderBtn.addEventListener("click", () => {
+    const finalCartStorage = JSON.parse(
+      localStorage.getItem("finalCartStorage")
+    );
+    //ajouter au storage détaillé (item + quantité)
+    if (!finalCartStorage) {
+      localStorage.setItem("finalCartStorage", JSON.stringify([finalCart]));
+    } else {
+      let storageCopy = [...finalCartStorage];
+
+      let idFound = null;
+
+      for (let i = 0; i < storageCopy.length; i++) {
+        if (selectedProduct[1]._id === storageCopy[i][1]._id) {
+          idFound = i;
+          break;
+        }
+      }
+
+      if (idFound !== null) {
+        // idFound !== null ---> pour éviter l'index = 0 qui renvoit une falsy value
+        storageCopy[idFound][0]++;
+        localStorage.setItem("finalCartStorage", JSON.stringify(storageCopy));
+        // console.log("yay");
+      } else {
+        let storageCopy = [...finalCartStorage, selectedProduct];
+        localStorage.setItem("finalCartStorage", JSON.stringify(storageCopy));
+      }
+    }
+  });
+};
+
+const displayCartNumberStorage = () => {
+  cartNumberStorage
+    ? ((cart.innerHTML = cartNumberStorage), (cart.style.display = "block"))
+    : (cart.style.display = "none");
+};
+
+const initPage = (() => {
+  displayProductPage();
+  addToCart();
+  addToStorage();
+  displayCartNumberStorage();
+})();
